9d9033795fa868a0c0088fc8296d20015fb246b6
Giovanni <giovanniyatze@gmail.com>
2016-03-04 22:57:38 -0600

    GPS

--- app/src/main/java/escobedo/jnix/escobedo/MapsActivity.java
+++ app/src/main/java/escobedo/jnix/escobedo/MapsActivity.java
@@ -2,17 +2,32 @@ package escobedo.jnix.escobedo;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private GPSTracker gps;
+    private UiSettings mUiSettings;
+
+    private AutoCompleteTextView from;
+
+    private double latitude;
+    private double longitude;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +51,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // create class object
+        gps = new GPSTracker(MapsActivity.this);
+
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+
+            // \n is for new line
+            //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng myLocation = new LatLng(latitude, longitude);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, mMap.getMaxZoomLevel() - 6));
+        mMap.setMyLocationEnabled(true);
+        mMap.setBuildingsEnabled(true);
+
+        mUiSettings=mMap.getUiSettings();
+        mUiSettings.setCompassEnabled(true);
+        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setMapToolbarEnabled(true);
+        mUiSettings.setMyLocationButtonEnabled(true);
+        //mUiSettings.setZoomControlsEnabled(true);
+
+        /*if ( getactualLocation <> myLocation) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(myLocation));
+        }*/
+
+
+        //mMap.addMarker(new MarkerOptions().position(myLocation).title("My Actual Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.cast_ic_notification_0)));
+
+        mUiSettings.isCompassEnabled();
+        mUiSettings.isMapToolbarEnabled();
     }
 }
